822f44d69a6178bdb0b6744d43e8576e75408c3d
yizhicheng <253921698@qq.com>
2015-06-05 15:40:06 +0800

    添加联系我们

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ exports.set = function(router){
     router.get('/home', function(req, res, next) {
         res.render('index', { title: 'Home' });
     });
+    router.get('/contact', function(req, res, next) {
+        // test微信
+
+        res.render('/contact', { title: '联系我们' });
+    });
 };
 
 
--- routes/yellowpages.js
+++ routes/yellowpages.js
@@ -1,5 +1,7 @@
 exports.set = function(router) {
     router.get('/yellowPages/list', function (req, res, next) {
+        
+        
         res.render('yellowPageslist', { title: '黄页列表' });
     });
 }
